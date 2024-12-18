f4cce62db46b1e1f0b4c5c02d2292b6da8dac65d
Doshi <doshi@Doshis-MacBook-Pro.local>
2024-12-18 16:35:48 -0500

    User: Content type error. To Fix.

--- backend/newsletter/src/startup.rs
+++ backend/newsletter/src/startup.rs
@@ -1,5 +1,7 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web::Form, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 use tracing_actix_web::TracingLogger;
+use uuid::Uuid;
 
 #[get("/")]
 pub async fn hello_world() -> impl Responder {
@@ -7,12 +9,63 @@ pub async fn hello_world() -> impl Responder {
         .body("Hello World.")
 }
 
+#[get("/name")]
+pub async fn name() -> impl Responder {
+    HttpResponse::Ok()
+        .body("name")
+}
+
+#[get("/getid")]
+pub async fn get_id() -> impl Responder {
+    HttpResponse::Ok()
+        .body(format!("Id={}", Uuid::new_v4().to_string()))
+}
+
+#[get("/user")]
+pub async fn get_user(_user: Form<User>) -> impl Responder {
+    HttpResponse::Ok()
+        .body("user")
+}
+
+#[derive(Deserialize)]
+pub struct User {
+    first_name: String,
+    last_name: String,
+}
+
+// The main reason for using methods instead of functions is for organization, 
+// in addition to providing method syntax and not having to repeat the type of self in every method’s signature
+impl User {
+    // ithin an impl block, the type Self is an alias for the type that the impl block is for
+    pub fn build_user() -> User {
+        User {
+            first_name: String::from("George"), 
+            last_name: String::from("Armani")
+        }
+    }
+
+    pub fn build_custom_user(first_name: String, last_name: String) -> User {
+        User {
+            first_name,
+            last_name,
+        }
+    }
+}
+
+
+pub struct FirstName(String);
+
+pub struct LastName(String);
+
 // !src/startup.rs
 pub async fn run() -> std::io::Result<()> {
     HttpServer::new( move || {
         App::new()
             .wrap(TracingLogger::default())
             .service(hello_world)
+            .service(name)
+            .service(get_id)
+            .service(get_user)
     })
     .bind(("127.0.0.1", 8000))?
     .run()
