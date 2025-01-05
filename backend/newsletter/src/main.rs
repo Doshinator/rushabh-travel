69e08c6a1c0fe0050788bb003de3159f6a906e31
Rushabh Doshi <doshi@Rushabhs-MacBook-Pro.local>
2025-01-05 14:08:57 -0500

    loops chapter finished

--- backend/newsletter/src/main.rs
+++ backend/newsletter/src/main.rs
@@ -1,15 +1,8 @@
 // !src/main
 
-use newsletter::loops;
-// use std::{fs::File, io::ErrorKind};
-// use newsletter::startup::run;
-// use newsletter::errors::open_file;
+use newsletter::startup::run;
 
-// #[tokio::main]
-// async fn main() -> std::io::Result<()>  {
-//     run().await
-// }
-
-fn main() {
-    loops::loops();
+#[tokio::main]
+async fn main() -> std::io::Result<()>  {
+    run().await
 }
